1507da306174f94efed1b5776bfba8f856e8f46d
jisueshin <126222350+jisueshin@users.noreply.github.com>
2024-02-12 17:14:26 -0500

    changed arrays to objects for dropdown menu

--- solution-hw3/hw3.js
+++ solution-hw3/hw3.js
@@ -1,7 +1,32 @@
 // filling glazing dropdown with glazing optoins
 let selectedGlazing = document.querySelector("#glazing");
 
-allGlazing = [
+allGlazing = {
+    "Keep original" : 0,
+    "Sugar milk" : 0,
+    "Vanilla milk" : 0.5,
+    "Double chocolate" : 1.5,
+}
+
+
+/*for (let i = 0; i < allGlazing.length; i++){
+    var option = document.createElement('option');
+    option.text = allGlazing[i].glazeName;
+    option.value = allGlazing[i].priceAdaptation;  
+    selectedGlazing.add(option);
+}*/
+
+//glazeKeys = Object.keys(allGlazing)
+//glazeValues = Object.values(allGlazing) 
+
+for (let [key, values] of Object.entries(allGlazing)){
+    var option = document.createElement('option');
+    option.text = key;  
+    option.value = values;  
+    selectedGlazing.add(option);
+}
+
+/*allGlazing = [
 {
     glazeName: "Keep original",
     priceAdaptation: 0,
@@ -18,20 +43,28 @@ allGlazing = [
     glazeName: "Double chocolate",
     priceAdaptation: 1.5,
 }
-]
+]*/
+//key and value 
 
-for (let i = 0; i < allGlazing.length; i++){
+/*for (let i = 0; i < allGlazing.length; i++){
     var option = document.createElement('option');
     option.text = allGlazing[i].glazeName;
     option.value = allGlazing[i].priceAdaptation;  
     selectedGlazing.add(option);
-}
+}*/
 
 //filling pack size dropdown with pack size options
 
 let selectedPack = document.querySelector("#pack-size");
 
-allPackSize = [
+allPackSize = {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10,
+}
+
+/*allPackSize = [
 {
     packSize: "1",
     priceAdaptation: 1,
@@ -48,14 +81,21 @@ allPackSize = [
     packSize: "12",
     priceAdaptation: 10,
 }
-]
+]*/
 
-for (let i = 0; i < allPackSize.length; i++){
+for (let [key, values] of Object.entries(allPackSize)){
+    var option = document.createElement('option');
+    option.text = key;  
+    option.value = values;  
+    selectedPack.add(option);
+}
+
+/*for (let i = 0; i < allPackSize.length; i++){
     var option = document.createElement('option');
     option.text = allPackSize[i].packSize;
     option.value = allPackSize[i].priceAdaptation;  
     selectedPack.add(option);
-}
+}*/
 
 // adding event listeners to detect when someone changes options
 selectedGlazing.addEventListener('change', onGlazingChange);
